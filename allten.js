8111f970c014ca893b29dc77b72dc82f49aacce7
Ben Trefry <bentref@gmx.com>
2024-09-06 15:10:24 -0700

    Parentheses working elegantly

--- allten.js
+++ allten.js
@@ -12,9 +12,9 @@ class Expr {
 }
 
 
-function checkNum(x) {
+function checkNum(x, max) {
 	x = Number(x);
-	if (isNaN(x) || x < 0 || x > 9) {
+	if (isNaN(x) || x < 0 || x > max) {
 		throw EvalError("You have entered an invalid number");
 	}
 	return x;
@@ -32,10 +32,10 @@ async function takeInput() {
 function parseInput(value) {
 	var inputGoal = value[0];
 	var inputNums = value[1];
-	var goal = checkNum(inputGoal);
+	var goal = checkNum(inputGoal, 10);
 	var numbers = inputNums.split(', ');
 	numbers.forEach(function(x, index) {
-		checkNum(x);
+		checkNum(x, 9);
 	});
 	// Close interface and call solver
 	console.log(createNumHelper(goal, numbers));
@@ -45,11 +45,6 @@ function parseInput(value) {
 
 const operators = ['*', '+', '-', '/'];
 
-function evalOrCreateExprs(goal, expr) {
-	if (expr.remaining.length == 0 && expr.getValue() == goal) {
-		return expr;
-	}
-}
 
 function createExprs(goal, expr) {
 	var poss_results = [expr]
@@ -64,9 +59,13 @@ function createExprs(goal, expr) {
 			poss_results = poss_results.concat(createExprs(goal, newExpr));
 			
 			// Also handle parentheses
-			let inners = createExprs(goal, new Expr(x, new_remaining));
+			let inners = createExprs(NaN, new Expr(x, new_remaining));
 			inners.forEach(function(inner, index) {
-				newExpr = new Expr(expr.expr + op + '(' + inner.expr + ')', inner.remaining);
+				if (inner.expr.length > 1) {
+					newExpr = new Expr(expr.expr + op + '(' + inner.expr + ')', inner.remaining);
+				} else {
+					newExpr = new Expr(expr.expr + op + inner.expr, inner.remaining);
+				}
 				poss_results = poss_results.concat(createExprs(goal, newExpr))
 			});
 			
@@ -82,20 +81,19 @@ function createNumHelper(goal, numbers) {
 	var poss_results = [];
 	numbers.forEach(function(x, index) {
 		var results = createExprs(goal, new Expr(x, numbers.toSpliced(index, 1)));
-		if (results.length > 0) {
-			poss_results = poss_results.concat(results);
-		}
+		poss_results = poss_results.concat(results);
 	});
-	return poss_results;
+	poss_results = poss_results.filter((expr) => expr.getValue() == goal && expr.remaining.length == 0);
+	return poss_results[0].expr;
 }
 
 function runOnce() {
 	takeInput()
 		.then(parseInput)
-		//.catch((e) => {
-			//console.error(e.message);
-			//runOnce();
-		//});
+		.catch((e) => {
+			console.error(e.message);
+			runOnce();
+		});
 }
 
 runOnce();
